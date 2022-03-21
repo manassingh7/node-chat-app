bf4671bdc983c95cebafe107ccfc739fbb13dc1f
Manas Singh <singh.mmanas@gmail.com>
2022-03-21 13:32:48 +0530

    Init commit

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ io.on("connection", (socket) => {
       "locationMessage",
       generateLocationMessage(
         user.username,
-        `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+        `HTTPS://google.com/maps?q=${coords.latitude},${coords.longitude}`
       )
     );
     callback();
--- src/utils/users.js
+++ src/utils/users.js
@@ -1,57 +1,55 @@
-const users = [];
-
-//addUser, removeUser ,getUser, getUsersInRoom
+const users = []
 
 const addUser = ({ id, username, room }) => {
-  // clear the data
-  username = username.trim().toLowerCase();
-  room = room.trim().toLowerCase();
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
 
-  // validate the data
-  if (!username || !room) {
-    return {
-      error: "Username & room are required!",
-    };
-  }
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
 
-  // check for existing user
-  const existingUser = users.find((user) => {
-    return user.room === room && user.username === username;
-  });
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
 
-  //validate Username
-  if (existingUser) {
-    return {
-      error: "Username is in use",
-    };
-  }
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
 
-  // Store user
-  const user = { id, username, room };
-  users.push(user);
-  return { user };
-};
+    // Store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
 
 const removeUser = (id) => {
-  const index = users.findIndex((user) => user.id === id);
+    const index = users.findIndex((user) => user.id === id)
 
-  if (index !== -1) {
-    return users.splice(index, 1)[0];
-  }
-};
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
 
 const getUser = (id) => {
-  return users.find((user) => user.id === id);
-};
+    return users.find((user) => user.id === id)
+}
 
 const getUsersInRoom = (room) => {
-  room = room.trim().toLowerCase();
-  return users.filter((user) => user.room === room);
-};
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
 
-module.exports ={
-  addUser,
-  removeUser,
-  getUser,
-  getUsersInRoom
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
 }
